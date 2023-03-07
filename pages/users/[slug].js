9300f9cf74b5c3598e1f733f272d1e48e35cf433
Moses Fawole <mosesfawole2001@gmail.com>
2023-03-07 17:02:36 +0100

    initial dynamic routing

--- pages/users/[slug].js
+++ pages/users/[slug].js
@@ -1,3 +1,21 @@
+export const getStaticPath = async () => {
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const paths = data.map((user) => {
+    return {
+      params: { id: user.id.toStrig() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const Details = () => {
   return (
     <div>
