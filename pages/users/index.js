f8bef8808cf1a67f47e76ec5db68f65e8bb00f02
Moses Fawole <mosesfawole2001@gmail.com>
2023-03-07 16:14:30 +0100

    fetch data(getStaticProps)

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import Link from "next/link";
+export const getStaticProps = async () => {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const res = await fetch(url);
+  const data = await res.json();
 
-const Users = () => {
-  return <div>All Users</div>;
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
+
+const Users = ({ users }) => {
+  return (
+    <div>
+      <h1>All Users</h1>
+
+      {users.map((user) => (
+        <div className="" key={user.id}>
+          <Link className="" href={"#"}>
+            <h3>{user.name}</h3>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Users;
