43ada5bdc5021928cd3a2d7f645dc5f8c7175624
Moses Fawole <mosesfawole2001@gmail.com>
2023-03-07 15:25:46 +0100

    add footer styles

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Copyright 2023 User List</div>;
+  return <footer>Copyright 2023 User List</footer>;
 };
 
 export default Footer;
