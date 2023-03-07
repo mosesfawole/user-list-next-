a1fa5104273fc713080d5cca1344723c1d0247e0
Moses Fawole <mosesfawole2001@gmail.com>
2023-03-07 17:15:01 +0100

    fetching a single item

--- pages/users/[slug].js
+++ pages/users/[slug].js
@@ -1,4 +1,4 @@
-export const getStaticPath = async () => {
+export const getStaticPaths = async () => {
   const url = "https://jsonplaceholder.typicode.com/users";
 
   const res = await fetch(url);
@@ -6,7 +6,7 @@ export const getStaticPath = async () => {
 
   const paths = data.map((user) => {
     return {
-      params: { id: user.id.toStrig() },
+      params: { slug: user.id.toString() },
     };
   });
 
@@ -16,10 +16,25 @@ export const getStaticPath = async () => {
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.slug;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      user: data,
+    },
+  };
+};
+
+const Details = ({ user }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+      <p>{user.website}</p>
+      <p>{user.address.city}</p>
     </div>
   );
 };
